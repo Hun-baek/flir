500055b65912465893d05aeadfe787b2ef6b5cef
One <82414916+Hun-baek@users.noreply.github.com>
2023-03-30 19:42:42 +0200

    Add pytest for data management functions

--- src/flir/config.py
+++ src/flir/config.py
@@ -4,9 +4,10 @@ from pathlib import Path
 SRC = Path(__file__).parent.resolve()
 BLD = SRC.joinpath("..", "..", "bld").resolve()
 PAPER_DIR = SRC.joinpath("..", "..", "paper").resolve()
+TEST_DIR = SRC.joinpath("..", "..", "test").resolve
 
 BASIS = ["bspline", "fourier"]
 GROUP = ["LBMP", "wind"]
 CONSTRAINT = ["none", "second_derivative", "harmonic"]
 
-__all__ = ["BLD", "SRC", "TEST_DIR", "BASIS", "GROUP", "CONSTRAINT"]
+__all__ = ["BLD", "SRC", "TEST_DIR", "TEST_DIR", "BASIS", "GROUP", "CONSTRAINT"]
--- tests/data_management/test_clean_data.py
+++ tests/data_management/test_clean_data.py
@@ -1,14 +1,23 @@
-import numpy as np
-import pandas as pd
 import pytest
+
 from flir.config import TEST_DIR
-from flir.data_management import clean_data
+from flir.data_management.clean_data import clean_consumption, clean_LBMP, clean_source
 from flir.utilities import read_yaml
 
 
 @pytest.fixture()
-def data():
-    return pd.read_csv(TEST_DIR / "data_management" / "data_fixture.csv")
+def consumption_path():
+    return TEST_DIR / "data_management" / "consumption_mock.csv"
+
+
+@pytest.fixture()
+def LBMP_path():
+    return TEST_DIR / "data_management" / "LBMP_mock.csv"
+
+
+@pytest.fixture()
+def wind_path():
+    return TEST_DIR / "data_management" / "wind_mock.csv"
 
 
 @pytest.fixture()
@@ -16,35 +25,18 @@ def data_info():
     return read_yaml(TEST_DIR / "data_management" / "data_info_fixture.yaml")
 
 
-def test_clean_data_drop_columns(data, data_info):
-    data_clean = clean_data(data, data_info)
-    assert not set(data_info["columns_to_drop"]).intersection(set(data_clean.columns))
-
-
-def test_clean_data_dropna(data, data_info):
-    data_clean = clean_data(data, data_info)
-    assert not data_clean.isna().any(axis=None)
-
-
-def test_clean_data_categorical_columns(data, data_info):
-    data_clean = clean_data(data, data_info)
-    for cat_col in data_info["categorical_columns"]:
-        cat_col = data_info["column_rename_mapping"].get(cat_col, cat_col)
-        assert data_clean[cat_col].dtype == "category"
-
-
-def test_clean_data_column_rename(data, data_info):
-    data_clean = clean_data(data, data_info)
-    old_names = set(data_info["column_rename_mapping"].keys())
-    new_names = set(data_info["column_rename_mapping"].values())
-    assert not old_names.intersection(set(data_clean.columns))
-    assert new_names.intersection(set(data_clean.columns)) == new_names
-
-
-def test_convert_outcome_to_numerical(data, data_info):
-    data_clean = clean_data(data, data_info)
-    outcome_name = data_info["outcome"]
-    outcome_numerical_name = data_info["outcome_numerical"]
-    assert outcome_numerical_name in data_clean.columns
-    assert data_clean[outcome_name].dtype == "category"
-    assert data_clean[outcome_numerical_name].dtype == np.int8
+def test_do_not_contain_summertimechange(
+    consumption_path, LBMP_path, wind_path, data_info
+):
+    consumption_clean = clean_consumption(consumption_path, data_info)
+    LBMP_clean = clean_LBMP(LBMP_path, data_info)
+    source_clean = clean_source(wind_path, data_info)
+    assert not set(data_info["summer_time_days"]).intersection(
+        set(consumption_clean.T.columns)
+    )
+    assert not set(data_info["summer_time_days"]).intersection(
+        set(LBMP_clean.T.columns)
+    )
+    assert not set(data_info["summer_time_days"]).intersection(
+        set(source_clean.T.columns)
+    )
